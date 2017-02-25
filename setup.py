1aadf55928619334bf4a1e62190d7478b0d4907a
Viktor Grandgeorg <info@intelligibel.de>
2017-02-25 18:42:48 +0100

    Add setup file

---
+++ setup.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+
+
+from setuptools import setup
+setup(
+    name='mdx_figurealtcaption',
+    version='1',
+    author='Jan Dittrich',
+    author_email='d _ jan AT ymail.com',
+    description='Generates a figurecaption each Image which stands alone in a paragraph',
+    url='https://github.com/grandgeorg/figureAltCaption',
+    py_modules=['mdx_figure_alt_caption'],
+    install_requires=['Markdown>=2.0',],
+    classifiers=[
+        'Development Status :: 1',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE Version 2',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'Topic :: Text Processing :: Filters',
+        'Topic :: Text Processing :: Markup :: HTML'
+    ]
+)
